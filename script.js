d62e4a4c732da601b0b431eef51b135da124403e
Denzel <denzelk741@gmail.com>
2023-12-26 13:01:51 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,104 @@
+const getData = async () => {
+  const response = await fetch('data.json');
+  const results = await response.json();
+  
+  return results;
+}
+
+getData()
+  .then(results => {
+    console.log("Promise resolved:", results)
+    const chartBox= document.querySelector('#chartBox').getContext('2d');
+    
+    //Statically generated data
+    /*let days = [
+      "mon",
+      "tue",
+      "wed",
+      "thur",
+      "fri",
+      "sat",
+      "sun",
+    ];*/
+    //let  spendings = [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48];
+    
+    //Dynamically generated data
+    var days = results.map(function(e) {
+      return e.day;
+    });
+    var spendings = results.map(function(e) {
+      return e.amount;
+    });
+    
+    let spendingChart = new Chart(chartBox, {
+      type: 'bar',
+      data: {
+        labels: days,
+        datasets: [
+          {
+          label: "Amount",
+          data: spendings,
+          backgroundColor: [
+            "hsl(10, 79%, 65%)",
+            "hsl(10, 79%, 65%)",
+            "hsl(186, 34%, 60%)",
+            "hsl(10, 79%, 65%)",
+            "hsl(10, 79%, 65%)",
+            "hsl(10, 79%, 65%)",
+            "hsl(10, 79%, 65%)",
+          ]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        
+        plugins:{
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              label: function(context) {
+                  let label = context.dataset.label || '';
+
+                  if (label) {
+                    label += ': ';
+                  }
+                  if (context.parsed.y !== null) {
+                    label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
+                }
+                return label;
+              }
+            }
+          }
+        },
+        
+        scales: {
+          y: {
+            display: false,
+            ticks: {
+              beginAtZero: true
+            }
+          }, 
+          x: {
+            grid: {
+              display: false,
+              drawBorder: false
+            }
+          }
+        },
+        
+        elements: {
+          bar: {
+            borderRadius: 4,
+            borderSkipped: false
+          }
+        }
+      }
+    });
+  })
+  .catch(err => console.log("Promise rejected", err));
+
+window.onload = getData;
